1a08f7803f6110d4c01675c37616471b11153908
mobilestudio <mobilestudio@mobilesudiosmbp.attlocal.net>
2016-03-30 10:03:05 -0700

    Initial code dev

--- calculatormax/ViewController.swift
+++ calculatormax/ViewController.swift
@@ -7,19 +7,76 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Equals = "="
+        case Empty = "Empty"
+    }
+    
+    @IBOutlet weak var calcDisplay: UILabel!
+    
+    var btnSound: AVAudioPlayer!
+    var runningNumber = ""
+    var leftVal = ""
+    var rightVal = ""
+    var currentOperation: Operation = Operation.Empty
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundUrl = NSURL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOfURL: soundUrl)
+            btnSound.prepareToPlay()
+            } catch let err as NSError {
+                print(err.debugDescription)
+            }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func numberPressed(btn: UIButton!) {
+        btnSound.play()
+        runningNumber += "\(btn.tag)"
+        calcDisplay.text = runningNumber
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func onEqualsPressed(sender: AnyObject) {
+        
+    }
+    
+    func processOperation(op: Operation) {
+        
+    }
 }
 
